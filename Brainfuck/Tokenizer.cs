cbdbf4c0cda16112c5f4447b9df1d8a3e84e4336
Vladislav Slobodenyuk <toastergithubmail@gmail.com>
2020-11-22 22:13:59 +0900

    Initial

---
+++ Brainfuck/Tokenizer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Brainfuck
+{
+    class Tokenizer
+    {
+        enum Token
+        {
+            Plus,
+            Minus,
+            PRight,
+            PLeft,
+            Output,
+            Input,
+            LoopOpen,
+            LoopClose
+        }
+
+        List<Token> Tokenize(string text)
+        {
+            List<Token> result = new List<Token>();
+        }
+    }
+}
