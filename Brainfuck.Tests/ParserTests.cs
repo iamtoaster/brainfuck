6cc83fb03633302091b8ea147b244c919035e76c
Vladislav Slobodenyuk <toastergithubmail@gmail.com>
2020-11-23 13:51:49 +0900

    Update Description, and properly commit parser

---
+++ Brainfuck.Tests/ParserTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using static Brainfuck.Parser;
+using static Brainfuck.Tokenizer;
+
+namespace Brainfuck.Tests
+{
+    public class ParserTests
+    {
+        [Fact]
+        public void BasicParse()
+        {
+            var input = "+++-[-]";
+            var tokens = Tokenizer.Tokenize(input);
+            RootAST expected = new RootAST();
+            expected.Children = new List<RootAST>( new RootAST[] { 
+                new ExprAST(Token.Plus),
+                new ExprAST(Token.Plus),
+                new ExprAST(Token.Plus),
+                new ExprAST(Token.Minus),
+                new LoopAST(new List<RootAST>(new RootAST[] { new ExprAST(Token.Minus) }))
+            });
+
+            Assert.Equal(expected, Parser.Parse(tokens));
+        }
+
+        [Fact]
+        public void ASTComparsion()
+        {
+            var left = new ExprAST(Token.Plus);
+            var right = new ExprAST(Token.Plus);
+            Assert.Equal(left, right);
+
+            var left1 = new RootAST(new List<RootAST>(new RootAST[] { new ExprAST(Token.Plus), new LoopAST(new List<RootAST>(new RootAST[] { new ExprAST(Token.Plus) })) }));
+            var right1 = new RootAST(new List<RootAST>(new RootAST[] { new ExprAST(Token.Plus), new LoopAST(new List<RootAST>(new RootAST[] { new ExprAST(Token.Plus) })) }));
+            Assert.Equal(left1, right1);
+
+            Assert.NotEqual(left, left1);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# brainfuck
+# Brainfuck Interpreter
+This is a small test project. This is a brainfuck language interpreter, that parses tokens into something that resembles a Abstract Syntax Tree, and then executes it.
