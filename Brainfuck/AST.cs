a6e6e4ffb1c5646b6314a6f635eb9a13f27b2141
Vladislav Slobodenyuk <toastergithubmail@gmail.com>
2020-11-23 13:48:28 +0900

    Parser Finished

--- Brainfuck/AST.cs
+++ Brainfuck/AST.cs
@@ -1,10 +1,137 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Brainfuck
 {
-    class AST
+    public class RootAST : IEquatable<RootAST>
     {
+        public List<RootAST> Children;
+        public RootAST Parent;
+
+        public RootAST()
+        {
+            Children = new List<RootAST>();
+        }
+
+        public RootAST(List<RootAST> children)
+        {
+            Children = children ?? throw new ArgumentNullException(nameof(children));
+        }
+
+        public RootAST(List<RootAST> children, RootAST parent) : this(children)
+        {
+            Parent = parent ?? throw new ArgumentNullException(nameof(parent));
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as RootAST);
+        }
+
+        public bool Equals(RootAST other)
+        {
+            return other != null &&
+                  Children.SequenceEqual(other.Children);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Children, Parent);
+        }
+
+        public static bool operator ==(RootAST left, RootAST right)
+        {
+            return EqualityComparer<RootAST>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(RootAST left, RootAST right)
+        {
+            return !(left == right);
+        }
+    }
+
+    public class ExprAST : RootAST, IEquatable<ExprAST>
+    {
+        public Tokenizer.Token Type;
+
+        public ExprAST(Tokenizer.Token type) : base()
+        {
+            Type = type;
+        }
+
+        public ExprAST(List<RootAST> children) : base(children)
+        {
+
+        }
+
+        public ExprAST(Tokenizer.Token type, List<RootAST> children) : base(children)
+        {
+            Type = type;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ExprAST);
+        }
+
+        public bool Equals(ExprAST other)
+        {
+            return other != null &&
+                   base.Equals(other) &&
+                   Type == other.Type;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(base.GetHashCode(), Type);
+        }
+
+        public static bool operator ==(ExprAST left, ExprAST right)
+        {
+            return EqualityComparer<ExprAST>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(ExprAST left, ExprAST right)
+        {
+            return !(left == right);
+        }
+    }
+    public class LoopAST : RootAST, IEquatable<LoopAST>
+    {
+        public LoopAST() : base()
+        {
+
+        }
+        public LoopAST(List<RootAST> children) : base(children)
+        {
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as LoopAST);
+        }
+
+        public bool Equals(LoopAST other)
+        {
+            return other != null &&
+                   base.Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(LoopAST left, LoopAST right)
+        {
+            return EqualityComparer<LoopAST>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(LoopAST left, LoopAST right)
+        {
+            return !(left == right);
+        }
     }
 }
---
+++ Brainfuck/Parser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static Brainfuck.Tokenizer;
+
+namespace Brainfuck
+{
+    public class Parser
+    {
+        public static RootAST Parse(List<Token> tokens)
+        {
+            RootAST result = new RootAST();
+
+            RootAST currentLoop = result;
+            foreach (var token in tokens)
+            {
+                switch (token)
+                {
+                    case Token.Plus:
+                        currentLoop.Children.Add(new ExprAST(Token.Plus));
+                        break;
+                    case Token.Minus:
+                        currentLoop.Children.Add(new ExprAST(Token.Minus));
+                        break;
+                    case Token.PRight:
+                        currentLoop.Children.Add(new ExprAST(Token.PRight));
+                        break;
+                    case Token.PLeft:
+                        currentLoop.Children.Add(new ExprAST(Token.PLeft));
+                        break;
+                    case Token.Output:
+                        currentLoop.Children.Add(new ExprAST(Token.Output));
+                        break;
+                    case Token.Input:
+                        currentLoop.Children.Add(new ExprAST(Token.Input));
+                        break;
+                    case Token.LoopOpen:
+                        var loop = new LoopAST();
+                        loop.Parent = currentLoop;
+                        currentLoop.Children.Add(loop);
+                        currentLoop = loop;
+                        break;
+                    case Token.LoopClose:
+                        currentLoop = currentLoop.Parent;
+                        break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
